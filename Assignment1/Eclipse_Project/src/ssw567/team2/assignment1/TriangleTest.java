a5b241869351508ddc95c6c8d512bc9371ed460c
conniejmiller <conniejmiller@yahoo.com>
2016-09-06 21:32:33 -0500

    Connie updated
    I believe this meets the needs of the assignment....or at least a good start

--- Assignment1/Eclipse_Project/src/ssw567/team2/assignment1/TriangleTest.java
+++ Assignment1/Eclipse_Project/src/ssw567/team2/assignment1/TriangleTest.java
@@ -1,14 +1,29 @@
 package ssw567.team2.assignment1;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-import org.junit.Test;
+public class TriangleTest extends TestCase {
+		
+	public void testClassifyTriangle() {
+		
+		Triangle t1 = new Triangle();
+		
+		assertEquals("Not a", t1.classifyTriangle(1,2,4));
+		assertEquals("Not a", t1.classifyTriangle(2,4,1));
+		assertEquals("Not a", t1.classifyTriangle(4,1,2));
+		
+		assertEquals("Equilateral", t1.classifyTriangle(3,3,3));
+		
+		assertEquals("Isosceles", t1.classifyTriangle(3,3,5));
+		assertEquals("Isosceles", t1.classifyTriangle(3,5,3));
+		assertEquals("Isosceles", t1.classifyTriangle(5,3,3));
+		
+		assertEquals("Scalene", t1.classifyTriangle(1,2,3));
+		assertEquals("Scalene", t1.classifyTriangle(2,3,1));
+		assertEquals("Scalene", t1.classifyTriangle(3,1,2));
 
-public class TriangleTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+		assertEquals("right Scalene", t1.classifyTriangle(3,4,5));	
+		assertEquals("right Scalene", t1.classifyTriangle(4,5,3));	
+		assertEquals("right Scalene", t1.classifyTriangle(5,3,4));	
 	}
-
 }
