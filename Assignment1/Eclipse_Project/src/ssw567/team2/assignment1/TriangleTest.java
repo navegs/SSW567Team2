9556c02e1a7ff2d2f79c3fe39039de0cd419d0cb
Scott Nave <scott.nave@gmail.com>
2016-09-08 10:32:27 -0400

    Added AssertTrue and AssertFalse assertions. Added failure descriptions to assertions

--- Assignment1/Eclipse_Project/src/ssw567/team2/assignment1/TriangleTest.java
+++ Assignment1/Eclipse_Project/src/ssw567/team2/assignment1/TriangleTest.java
@@ -1,6 +1,8 @@
 package ssw567.team2.assignment1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -18,25 +20,25 @@ public class TriangleTest {
 	@Test
 	public void isoscelesTest() {
 
-		assertEquals("Isosceles", tester.classifyTriangle(3,3,5));
-		assertEquals("Isosceles", tester.classifyTriangle(3,5,3));
-		assertEquals("Isosceles", tester.classifyTriangle(5,3,3));
+		assertEquals("failure - should be Isosceles", "Isosceles", tester.classifyTriangle(3,3,5));
+		assertEquals("failure - should be Isosceles", "Isosceles", tester.classifyTriangle(3,5,3));
+		assertEquals("failure - should be Isosceles", "Isosceles", tester.classifyTriangle(5,3,3));
 		
 	}
 
 	@Test
 	public void scaleneTest() {
 
-		assertEquals("Scalene", tester.classifyTriangle(1,2,3));
-		assertEquals("Scalene", tester.classifyTriangle(2,3,1));
-		assertEquals("Scalene", tester.classifyTriangle(3,1,2));
+		assertEquals("failure - should be Scalene", "Scalene", tester.classifyTriangle(1,2,3));
+		assertEquals("failure - should be Scalene", "Scalene", tester.classifyTriangle(2,3,1));
+		assertEquals("failure - should be Scalene", "Scalene", tester.classifyTriangle(3,1,2));
 
 	}
 
 	@Test
 	public void equilateralTest() {
 
-		assertEquals("Equilateral", tester.classifyTriangle(3,3,3));
+		assertEquals("failure - should be Equilateral", "Equilateral", tester.classifyTriangle(3,3,3));
 
 	}
 
@@ -44,35 +46,35 @@ public class TriangleTest {
 	public void invalidTriangleTest() {
 
 		// Negative Numbers for each parameter
-		assertEquals("Not a", tester.classifyTriangle(-1,4,8));
-		assertEquals("Not a", tester.classifyTriangle(4,-1,8));
-		assertEquals("Not a", tester.classifyTriangle(4,8,-1));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(-1,4,8));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(4,-1,8));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(4,8,-1));
 		
 		// Zeros for each parameter
-		assertEquals("Not a", tester.classifyTriangle(0,4,8));
-		assertEquals("Not a", tester.classifyTriangle(4,0,8));
-		assertEquals("Not a", tester.classifyTriangle(4,8,0));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(0,4,8));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(4,0,8));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(4,8,0));
 		
 		// Not a Triangle
-		assertEquals("Not a", tester.classifyTriangle(1,2,4));
-		assertEquals("Not a", tester.classifyTriangle(2,4,1));
-		assertEquals("Not a", tester.classifyTriangle(4,1,2));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(1,2,4));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(2,4,1));
+		assertEquals("failure - should be Not a", "Not a", tester.classifyTriangle(4,1,2));
 
 	}
 	
 	@Test
 	public void trueRightAngleTest() {
 		
-		assertEquals(true, tester.isRightAngle(3, 4, 5));
-		assertEquals(true, tester.isRightAngle(4, 5, 3));	
-		assertEquals(true, tester.isRightAngle(5, 3, 4));
+		assertTrue("failure - should be true", tester.isRightAngle(3, 4, 5));
+		assertTrue("failure - should be true", tester.isRightAngle(4, 5, 3));	
+		assertTrue("failure - should be true", tester.isRightAngle(5, 3, 4));
 
 	}
 	
 	@Test
 	public void falseRightAngleTest() {
 		
-		assertEquals(false, tester.isRightAngle(2, 4, 6));
+		assertFalse("failure - should be false", tester.isRightAngle(2, 4, 6));
 
 	}
 	
